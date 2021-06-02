9dffb623691b0c484d4a49471fa3f1a9ad0a397b
Martin Azpillaga Aldalur <martin@minim.tools>
2021-06-03 00:31:24 +0200

    Notification worker and delayed notifications

---
+++ app/src/main/java/com/fisify/app/NotificationWorker.java
@@ -0,0 +1,35 @@
+package com.fisify.app;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class NotificationWorker extends Worker
+{
+    Context context;
+    public NotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams)
+    {
+        super(context, workerParams);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Result doWork()
+    {
+        Intent intent = new Intent(context, Main.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        Notification notification = new NotificationCompat.Builder(context, "FISIFY_CHANNEL_ID").setSmallIcon(R.drawable.logo).setContentTitle("Fisify").setContentText("Your next session is ready").setPriority(NotificationCompat.PRIORITY_MAX).setContentIntent(pendingIntent).setAutoCancel(true).build();
+        NotificationManagerCompat.from(context).notify(1, notification);
+
+        return Result.success();
+    }
+}
