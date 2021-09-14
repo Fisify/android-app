2c249ea48e4f0cd53c0f3617ec459e11f80bf9de
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2021-09-14 12:20:09 +0200

    Import fixed.

--- app/src/main/java/com/fisify/app/Main.java
+++ app/src/main/java/com/fisify/app/Main.java
@@ -9,7 +9,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.webkit.JavascriptInterface;
